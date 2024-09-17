30e0149fe1ff1b9fa339e50d2813c8d50cd3d014
agoebelknapp <angelina.goknapp@gmail.com>
2024-09-17 11:42:08 +0200

    Further tests on preprocess.R

---
+++ tests/testthat/test-analyze.R
@@ -0,0 +1,3 @@
+test_that("mpra_treat", {
+  expect_equal(2 * 2, 4)
+})
--- tests/testthat/test-preprocess.R
+++ tests/testthat/test-preprocess.R
@@ -1,3 +1,9 @@
+library(mpra)
+library(testthat)
+library(usethis)
+library(devtools)
+library(tidyr)
+library(dplyr)
 ##Data preparation for tests
 
 #Downsample_barcodes
@@ -21,25 +27,79 @@ single_column_df <- data.frame(
 
 #create_var_df
 df_var_1 <- data.frame(
-  name = c("var1", "var2", "var3", "var4"),
-  Barcode = c("BC1", "BC2", "BC3", "BC4"),
-  count = c(10, 20, 30, 40)
+  name = c("A", "B", "C"),
+  Barcode = c("BC1", "BC2", "BC3"),
+  count = c(10, 20, 30)
 )
 
 map_df_var_1 <- data.frame(
   ID = c("var1", "var2", "var3", "var4"),
-  REF = c("var1", "var3", "var5", "var7"),
-  ALT = c("var2", "var4", "var6", "var8")
+  REF = c("A", "A", "B", "D"),
+  ALT = c("E", "F", "G", "C")
 )
 
 res_var_1 <- data.frame(
-  var_id = c("var1", "var2", "var3", "var4"),
-  allele = c("ref", "ref", "alt", "alt"),
-  Barcode = c("BC1", "BC2", "BC3", "BC4"),
-  code = c(10, 20, 30, 40)
+  variant_id = c("var1", "var2", "var3", "var4"),
+  allele = c("ref", "ref", "ref", "alt"),
+  Barcode = c("BC1", "BC1", "BC2", "BC3"),
+  count = c(10, 10, 20, 30)
 )
 
-##testthat functions
+map_df_var_2 <- data.frame(
+  ID = c("var1", "var2", "var3", "var4"),
+  REF = c("U", "V", "W", "X"),
+  ALT = c("Y", "Z", "Q", "R")
+)
+
+res_var_2 <- data.frame(
+  variant_id = character(),
+  allele = character(),
+  Barcode = character(),
+  count = numeric()
+)
+
+#create_dna_df
+df_dna_1 <- data.frame(
+  variant_id = c("var1", "var2"),
+  allele = c("ref", "alt"),
+  DNA_A = c(100, 200),
+  DNA_B = c(300, 400))
+
+res_dna_1 <- data.frame(
+  sample_A_bc1_ref = c(100, NA),
+  sample_A_bc1_alt = c(NA, 200),
+  sample_B_bc1_ref = c(300, NA),
+  sample_B_bc1_alt = c(NA, 400),
+  row.names = c("var1", "var2"))
+
+df_dna_2 <- data.frame(
+  variant_id = c("var1", "var2"),
+  custom_allele = c("ref", "alt"),
+  DNA_A = c(100, 200),
+  DNA_B = c(300, 400))
+
+res_dna_2 <- data.frame(
+  sample_A_bc1_ref = c(100, NA),
+  sample_A_bc1_alt = c(NA, 200),
+  sample_B_bc1_ref = c(300, NA),
+  sample_B_bc1_alt = c(NA, 400),
+  row.names = c("var1", "var2"))
+
+df_dna_3 <- data.frame(
+  DNA_A = c(100, 200),
+  DNA_B = c(300, 400))
+
+df_dna_4 <- data.frame(
+  variant_id = character(),
+  DNA_A = numeric(),
+  DNA_B = numeric())
+
+df_dna_5 <- data.frame(
+  name = c("var1", "var2"),
+  allele = c("ref", "alt"),
+  DNA_A = c(100, 200))
+
+##testthat calls
 test_that("downsample_barcodes", {
 
   expect_equal(as.data.frame(downsample_barcodes(empty_df)), empty_df)
@@ -50,6 +110,17 @@ test_that("downsample_barcodes", {
 
 test_that("create_var_df", {
 
-  expect_equal(as.data.frame(create_var_df(df_var_1,map_df_var_1)), res_var_1)
+  expect_equal(create_var_df(df_var_1,map_df_var_1), res_var_1)
+  expect_equal(create_var_df(df_var_1,map_df_var_2), res_var_2)
+
+})
+
+test_that("create_dna_df", {
+  
+  expect_equal(create_dna_df(df_dna_1), res_dna_1)
+  expect_equal(create_dna_df(df_dna_2, allele_column_name = "custom_allele"), res_dna_2)
+  expect_error(create_dna_df(df_dna_3))
+  expect_equal(create_dna_df(df_dna_4), data.frame())
+  expect_error(create_dna_df(df_dna_5, id_column_name = "variant_id"))
 
 })
