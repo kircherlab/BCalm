c1c3dd14673ef23c0d2b9a316b5405cbf86ccb91
agoebelknapp <angelina.goknapp@gmail.com>
2025-04-07 16:45:39 +0200

    Changed input to list of replicates; removed variant testing support

--- R/rep_correlation.r
+++ R/rep_correlation.r
@@ -1,30 +1,17 @@
-replicate_fit <- function(mpra, block_vector){
-    nr_reps <- length(unique(block_vector))
-    dna <- getDNA(mpra)
-    rna <- getRNA(mpra)
-    labels <- getLabel(mpra)
-    bcs <- ncol(dna)/nr_reps
-    result <- data.frame(oligo_names = rownames(dna))
+replicate_fit <- function(rep_list){
+    nr_reps <- length(rep_list)
+    bcs <- ncol(rep_list$rep1$DNA) # Acsess to the actual dataframe and size, assume every rep has same number of bcs?
+    result <- data.frame(oligo_names = rownames(rep_list$rep1$DNA))
     for (i in 1:nr_reps){
         # create new MPRASets for each replicate
-        rep_dna <- dna[,(bcs*(i-1)+1):(bcs*i)]
-        rep_rna <- rna[,(bcs*(i-1)+1):(bcs*i)]
-        rep_block_vec <- rep(1:nr_reps, each=bcs)
-        # different cases if mpra data was variant or element testing data
-        if (grepl("ref|alt", colnames(dna)[1])) {
-            rep_mpra <- MPRASet(DNA = rep_dna, RNA = rep_rna, eid = row.names(rep_dna), barcode = NULL)
-            rep_design <- data.frame(intcpt = 1, alt = grepl("alt", colnames(rep_mpra)))
-            # fit replicate with "indep_groups"
-            rep_mpralm_fit <- mpralm(object = rep_mpra, design = rep_design, aggregate = "none", normalize = TRUE, model_type = "indep_groups", plot = FALSE, block = rep_block_vec)
-            top_var <- topTable(rep_mpralm_fit, coef = 2, number = Inf)
-        } else {
-            labels_vec <- labels[rownames(rep_dna)]
-            rep_mpra <- MPRASet(DNA = rep_dna, RNA = rep_rna, eid = row.names(rep_dna), barcode = NULL)
-            rep_mpralm_fit <- fit_elements(object = rep_mpra, normalize=TRUE, block = rep_block_vec, endomorphic = FALSE, plot = FALSE)
-            top_var <- topTable(rep_mpralm_fit, coef = 1, number = Inf)
-        }
+        rep_dna <- rep_list[[i]]$DNA
+        rep_rna <- rep_list[[i]]$RNA
+        rep_block_vec <- rep(1, each=bcs)
+        rep_mpra <- MPRASet(DNA = rep_dna, RNA = rep_rna, eid = row.names(rep_dna), barcode = NULL)
+        rep_mpralm_fit <- fit_elements(object = rep_mpra, normalize=TRUE, block = rep_block_vec, endomorphic = FALSE, plot = FALSE)
+        top_var <- topTable(rep_mpralm_fit, coef = 1, number = Inf)
         # put into requested dataframe form
-        top_var <- top_var[rownames(dna), , drop = FALSE]
+        top_var <- top_var[rownames(rep_dna), , drop = FALSE]
         col_name <- paste0("logFC_", i)
         result[[col_name]] <- top_var$logFC
     }
