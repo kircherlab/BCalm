1ac135dc0b6bc85c563b932e8fbb3f8670b338e4
Pia Keukeleire <piakeuk@hotmail.com>
2025-10-17 15:37:37 +0200

    change fit_elements

--- R/fit.R
+++ R/fit.R
@@ -1,19 +1,12 @@
 fit_elements <- function(object, normalize=TRUE, block = NULL, endomorphic=FALSE, normalizeSize=1e9, ...) {
 	design <- data.frame(rep(1, ncol(object)))
-	if (normalize) {
-		if ("normalizeSize" %in% names(formals(normalize_counts))) {
-			object <- normalize_counts(object, normalizeSize=normalizeSize, block=block)
-		} else {
-			object <- normalize_counts(object, block=block)
-		}
-	}
 	if ("endomorphic" %in% names(formals(mpralm))) {
 		mpralm_fit <- mpralm(object = object, design = design, aggregate = "none", 
-						normalize = F, model_type = "indep_groups", 
+						normalize = T, model_type = "corr_groups", 
 						block = block, endomorphic = endomorphic, normalizeSize = normalizeSize, ...)
 	} else {
 		mpralm_fit <- mpralm(object = object, design = design, aggregate = "none", 
-						normalize = F, model_type = "indep_groups", 
+						normalize = T, model_type = "corr_groups", 
 						block = block, ...)
 	}
 	if (! endomorphic) {
